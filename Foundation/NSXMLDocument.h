48d25dc082c8d3b3fea6e9683324efdf4fa8826c
H. Nikolaus Schaller <hns@goldelico.com>
2008-03-30 18:10:15 +0000

    NSXMLNode and subclasses added

---
+++ Foundation/NSXMLDocument.h
@@ -0,0 +1,17 @@
+//
+//  NSXMLDocument.h
+//  Foundation
+//
+//  Created by H. Nikolaus Schaller on 28.03.08.
+//  Copyright 2008 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+
+#import <Foundation/NSXMLNode.h>
+
+
+@interface NSXMLDocument : NSXMLNode
+{
+
+}
+
+@end
---
+++ Foundation/NSXMLNode.m
@@ -0,0 +1,14 @@
+//
+//  NSXMLNode.m
+//  Foundation
+//
+//  Created by H. Nikolaus Schaller on 28.03.08.
+//  Copyright 2008 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+
+#import "NSXMLNode.h"
+
+
+@implementation NSXMLNode
+
+@end
