98c07833c2e57fc7e00fbeae0c9fbf6935be3dcf
H. Nikolaus Schaller <hns@goldelico.com>
2010-10-09 15:30:34 +0000

    implemented first steps of CoreWLAN

---
+++ CoreWLAN/CoreWLAN.h
@@ -0,0 +1,14 @@
+//
+//  CoreWLAN.h
+//  CoreWLAN
+//
+//  Created by H. Nikolaus Schaller on 03.10.10.
+//  Copyright 2010 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+
+#import <CoreWLAN/CW8021XProfile.h>
+#import <CoreWLAN/CWConfiguration.h>
+#import <CoreWLAN/CWGlobals.h>
+#import <CoreWLAN/CWInterface.h>
+#import <CoreWLAN/CWNetwork.h>
+#import <CoreWLAN/CWWirelessProfile.h>
---
+++ CoreWLAN/CoreWLAN.m
@@ -0,0 +1,21 @@
+//
+//  CoreWLAN.m
+//  CoreWLAN
+//
+//  Created by H. Nikolaus Schaller on 03.10.10.
+//  Copyright 2010 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+
+/*
+  Here is some API:
+		http://developer.apple.com/library/mac/#documentation/Networking/Reference/CoreWLANFrameworkRef/
+ 
+  Examples how to use see e.g.
+		http://dougt.org/wordpress/2009/09/usingcorewlan/
+		http://lists.apple.com/archives/macnetworkprog/2009/Sep/msg00007.html
+ 
+ */
+
+#import <Foundation/Foundation.h>
+#import <CoreWLAN/CoreWLAN.h>
+
