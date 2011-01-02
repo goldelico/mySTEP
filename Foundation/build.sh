5eeddd4625eef4120a86f6b302f8b56a1ad2f6a7
H. Nikolaus Schaller <hns@goldelico.com>
2011-01-02 22:00:39 +0000

    cleanup

---
+++ Foundation/build.sh
@@ -0,0 +1,15 @@
+# project settings
+export SOURCES="Sources/NS*.m Sources/Externs.m Sources/Unicode.m ../SimpleWebKit/Sources/NSXMLParser.m"   # all source codes
+# export SOURCES="Sources/NSString.m Sources/Externs.m Sources/Unicode.m"   # all source codes
+export LIBS="-Uobjc_main -lm -ldl -lpthread -lssl -lcrypto -lobjc"   # add any additional libraries (or flags) like -ltiff etc.
+export FRAMEWORKS=				   # add any additional Frameworks etc.
+export INSTALL_PATH=/System/Library/Frameworks   # override INSTALL_PATH for MacOS X for Zaurus
+
+# global/compile settings
+#export INSTALL=true                # true (or empty) will install locally to $ROOT/$INSTALL_PATH
+#export SEND2ZAURUS=true		   # true (or empty) will try to install on the Zaurus at /$INSTALL_PATH (using ssh)
+#export RUN=true                    # true (or empty) will finally try to run on the Zaurus (using X11 on host)
+export DEPENDS="libc6 (>= 2.7-1), libobjc2, libssl0.9.8"      # debian package dependencies
+
+[ "$ROOT" ] || export ROOT=/usr/share/QuantumSTEP	# project root
+/usr/bin/make -f $ROOT/System/Sources/Frameworks/mySTEP.make $ACTION
---
+++ build.sh
@@ -0,0 +1,59 @@
+export BUILD_FOR_PACKAGE=true
+export SEND2ZAURUS=false
+export RUN=false
+ 
+echo ## Frameworks ##
+for i in Foundation AppKit CoreData CoreFoundation Cocoa AddressBook Message PreferencePanes SystemUIPlugin Security SecurityFoundation SecurityInterface IOBluetooth ImageKit PDFKit QTKit ScreenSaver
+	do
+	(cd $i && ./build.sh) )
+	done
+echo ## done. ##
+
+echo ## Extended Frameworks ##
+for i in SimpleWebKit SystemConfiguration SystemStatus Tools CoreLocation MapKit CoreWLAN CoreRFID
+	do
+	(cd $i && ./build.sh) )
+	done
+echo ## done. ##
+
+echo ## Special Frameworks ##
+false && for i in UIKit
+	do 
+	(cd $i && ./build.sh) )
+	done
+echo ## done. ##
+
+echo ## Tools ##
+for i in Tools CoreData/DataBuilder CoreRFID/RFIDProber
+	do
+	(cd $i && ./build.sh) )
+	done
+echo ## done. ##
+
+echo ## TestApps ##
+false && for i in TestApplications/NSImageRep TestApplications/NSTextTable TestApplications/NSURLConnection TestApplications/ZeroConfDistributedObjects
+	do
+	(cd $i && ./build.sh) )
+	done
+echo ## done. ##
+
+# project settings
+export SOURCES=   			  	 # no sources
+export LIBS=  				   # add any additional libraries (or flags) like -ltiff etc.
+export FRAMEWORKS=				   # add any additional Frameworks etc.
+# export FILES="./etc ./System ./usr"		# additional files to include in package (relative to project install path)
+# export DATA="./usr/bin/qssh ./usr/bin/qsx"	# additional files to include in package (relativ to root)
+export INSTALL_PATH=/			   # override INSTALL_PATH for MacOS X
+
+# global/compile settings
+#export INSTALL=true                # true (or empty) will install locally to $ROOT/$INSTALL_PATH
+#export SEND2ZAURUS=true		   # true (or empty) will try to install on the Zaurus at /$INSTALL_PATH (using ssh)
+#export RUN=true                    # true (or empty) will finally try to run on the Zaurus (using X11 on host)
+
+# debian package dependencies (, separated)
+# this excludes the Tools and TestApps!
+
+export DEPENDS="quantumstep-addressbook-framework, quantumstep-corefoundation-framework, quantumstep-iobluetooth-framework, quantumstep-iobluetoothui-framework, quantumstep-imagekit-framework, quantumstep-message-framework, quantumstep-qtkit-framework, quantumstep-preferencepanes-framework, quantumstep-screensaver-framework, quantumstep-securityinterface-framework, quantumstep-webkit-framework, quantumstep-systemstatus-framework, quantumstep-systemuiplugin-framework, quantumstep-corelocation-framework, quantumstep-mapkit-framework, quantumstep-corewlan-framework, quantumstep-corerfid-framework"
+
+[ "$ROOT" ] || export ROOT=/usr/share/QuantumSTEP	# project root
+/usr/bin/make -f $ROOT/System/Sources/Frameworks/mySTEP.make $ACTION
