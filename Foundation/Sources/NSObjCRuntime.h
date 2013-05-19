b36f3bb1b2f0c8725bfc10a985982c1e93b3aafc
H. Nikolaus Schaller <hns@goldelico.com>
2013-05-19 18:13:11 +0000

    fixed .qcodeproj and .xcodeproj

--- Foundation/Sources/NSObjCRuntime.h
+++ Foundation/Sources/NSObjCRuntime.h
@@ -18,6 +18,7 @@
 #define _mySTEP_H_NSObjCRuntime
 
 // include all important standard C libraries
+#define __EXPORTED_HEADERS__	// disable some warnings in gcc/Linux
 
 #include <stddef.h>
 #include <stdlib.h>
