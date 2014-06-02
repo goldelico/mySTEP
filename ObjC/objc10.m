553bfbe45b9831952494932efa86a7b896514249
H. Nikolaus Schaller <hns@goldelico.com>
2014-06-02 21:32:55 +0200

    ObjC: finally fixed the grammar to only have three known and expected shift/reduce conflicts: one for if/else and  the others for @catch/@catch and @catch/@finally
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- ObjC/objc10.m
+++ ObjC/objc10.m
@@ -18,14 +18,59 @@
 
 // check for idioms
 // . notation for KVC
-// @try, @catch
-// @synchronized
-// @synthesize
-// @autorelease
 // ARC
+
+- (void) compile_objc1synchronized
+{
+	// NSLock
+}
+
+- (void) compile_objc1synthesize
+{
+	// create getters/setters
+}
+
+- (void) compile_objc1autorelease
+{
+	// -> { NSAutoreleasePool *arp=[NSAutoreleasepool new]; $1; [arp release]; }
+}
 
 - (void) compile_objc1try
 {
+	// -> NS_DURING
+}
+
+- (void) compile_objc1catch
+{
+	// -> NS_HANDLER
+}
+
+- (void) compile_objc1finaly
+{
+	// -> NS_ENDHANDLER
+}
+
+- (void) compile_objc1box
+{
+	// box -> [NSNUmber numberWith<type>:$1]
+	
+}
+
+- (void) compile_objc1arrayliteral
+{
+	// arrayliteral -> { id values[]={$1,$2,...}; [NSArray arrayWithObjects:values count:$#] }
+	
+}
+
+- (void) compile_objc1dictliteral
+{
+	// dictliteral -> { id keys[]={$1,$3,...}, values[]={$2,$4,...}; [NSDictionary dictionaryWithObjects:values forKeys:keys count:$#/2] }
+	
+}
+
+- (void) compile_objc1index
+{
+	// handle <object>[index] -> [object objectAtIndex:index] or [object objectForKey:index] depending on whether index is an C integer or an object (id)
 	
 }
 
