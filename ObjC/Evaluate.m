83efeef7008d38a59027fe5260bb366c982f1324
H. Nikolaus Schaller <hns@goldelico.com>
2014-06-09 12:36:21 +0200

    ObjC: extended evaluator a little
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- ObjC/Evaluate.m
+++ ObjC/Evaluate.m
@@ -20,27 +20,81 @@
 
 @implementation Node (Evaluate)
 
-- (void) evaluate;
+- (id) evaluate;
 {
 	id machine=nil;
+#if 1
+	NSLog(@"evaluate %@", self);
+#endif
 	[self treeWalk:@"evaluate" withObject:machine];	// recursive
 }
 
-- (void) evaluate_default:(id) machine
+- (id) evaluate_default:(id) machine
 {
 	NSLog(@"can't evaluate");
+	return nil;
 }
 
-- (void) evaluate_const:(id) machine
+- (id) evaluate_const:(id) machine
 {
-	NSLog(@"can't evaluate");
-	// push value on stack
+	return self;
 }
 
-- (void) evaluate_ident:(id) machine
+- (id) evaluate_ident:(id) machine
 {
-	NSLog(@"can't evaluate");
-	// push reference to variable storage
+	// make a reference so that we can be a Lvalue
+	return self;
+}
+
+// FIXME: a block must open a fresh range of variables
+// or can we simply use the attributes of the identifier?
+
+- (id) fetch
+{
+	// if reference (e.g. identifier), getVar:name
+	return self;
+}
+
+- (void) store:(id) val
+{
+	// if not a reference -> not a Lvalue -> can't assign
+	// store value
+}
+
+- (id) evaluate_add:(id) machine
+{
+	// dereference children
+	// add [child intValue]
+	// return [NSNumber numberWithInt:sum];
+}
+
+- (id) evaluate_call:(id) machine
+{ // function call
+	// dereference children
+	// push on stack
+	// check for built-in (NSLog)
+	// call function
+	// return result
+}
+
+- (id) evaluate_methodcall:(id) machine
+{
+	// dereference children
+	SEL sel=@selector(selectorOfCalledMethod);
+	id target=nil;	// should be "self/super" of current method
+	NSMethodSignature *ms=[target methodSignatureForSelector:sel];
+	NSInvocation *i=[NSInvocation invocationWithMethodSignature:ms];
+	id ret;
+	[i setTarget:target];
+	[i setSelector:sel];
+	// setup method arguments
+	NS_DURING
+		[i invoke];
+	NS_HANDLER
+	// ???
+	NS_ENDHANDLER
+	[i getReturnValue:&ret];
+	return ret;
 }
 
 @end
