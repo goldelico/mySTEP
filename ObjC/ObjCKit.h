544747f5b46008fc068523fc872acf3598565ee9
H. Nikolaus Schaller <hns@goldelico.com>
2014-06-20 22:17:31 +0200

    ObjC: added Inspector to project files
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- ObjC/ObjCKit.h
+++ ObjC/ObjCKit.h
@@ -1,10 +1,12 @@
 #import <ObjCKit/AST.h>
+#import <ObjCKit/Postprocess.h>
+
+#import <ObjCKit/compile.h>
 #import <ObjCKit/Evaluate.h>
 #import <ObjCKit/Expression.h>
-#import <ObjCKit/Print.h>
-#import <ObjCKit/Simplify.h>
-#import <ObjCKit/Refactor.h>
 #import <ObjCKit/objc10.h>
 #import <ObjCKit/objc20.h>
 #import <ObjCKit/objc2c.h>
-#import <ObjCKit/compile.h>
+#import <ObjCKit/Print.h>
+#import <ObjCKit/Refactor.h>
+#import <ObjCKit/Simplify.h>
