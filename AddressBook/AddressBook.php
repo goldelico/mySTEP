caffc7e6e5e48bab0314c6d83163139a00384579
H. Nikolaus Schaller <hns@goldelico.com>
2016-03-27 17:32:01 +0200

    AddressBook.php: added empty source file
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

---
+++ AddressBook/AddressBook.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * AddressBook.framework
+ * (C) Golden Delicious Computers GmbH&Co. KG, 2016
+ * All rights reserved.
+ *
+ */
+
+global $ROOT;	// must be set by some .app
+require_once "$ROOT/System/Library/Frameworks/Foundation.framework/Versions/Current/php/Foundation.php";
+require_once "$ROOT/System/Library/Frameworks/AppKit.framework/Versions/Current/php/AppKit.php";
