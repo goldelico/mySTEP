3ae7a8f75b2e2fb79f996692484b1ea010741faa
H. Nikolaus Schaller <hns@goldelico.com>
2016-08-28 22:47:38 +0200

    SenTestingKit: InvocationEnumerator fixed
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- OCUnit/SourceCode/SenTestingKit/SenTestInvocationEnumerator.m
+++ OCUnit/SourceCode/SenTestingKit/SenTestInvocationEnumerator.m
@@ -3,29 +3,29 @@
 // Copyright (c) 1997-2005, Sen:te (Sente SA).  All rights reserved.
 //
 // Use of this source code is governed by the following license:
-// 
-// Redistribution and use in source and binary forms, with or without modification, 
+//
+// Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-// 
-// (1) Redistributions of source code must retain the above copyright notice, 
+//
+// (1) Redistributions of source code must retain the above copyright notice,
 // this list of conditions and the following disclaimer.
-// 
-// (2) Redistributions in binary form must reproduce the above copyright notice, 
-// this list of conditions and the following disclaimer in the documentation 
+//
+// (2) Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
 // and/or other materials provided with the distribution.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' 
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-// IN NO EVENT SHALL Sente SA OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
-// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL Sente SA OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 // EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // Note: this license is equivalent to the FreeBSD license.
-// 
+//
 // This notice may not be removed from this file.
 
 #import "SenTestInvocationEnumerator.h"
@@ -41,6 +41,7 @@
 - (void) goNextMethodList
 {
 #if defined (GNUSTEP) || defined(__mySTEP__)
+	// not used
 #else
 	mlist = class_nextMethodList (class, &iterator);
 	count = (mlist != NULL) ? mlist->method_count - 1 : -1;
@@ -50,12 +51,16 @@
 
 - (id) initForClass:(Class) aClass
 {
-//	NSLog(@"SenTestInvocationEnumerator initForClass:%@", NSStringFromClass(aClass));
-    self = [super init];
-    class = aClass;
-    iterator = NULL;
-    [self goNextMethodList];
-    return self;
+#if 1
+	NSLog(@"SenTestInvocationEnumerator initForClass:%@", NSStringFromClass(aClass));
+#endif
+	if(self = [super init])
+		{
+		class = aClass;
+		iterator = NULL;
+		[self goNextMethodList];
+		}
+	return self;
 }
 
 
@@ -64,55 +69,64 @@
 #if defined (GNUSTEP) || defined(__mySTEP__)
 	NSInvocation *invocation;
 	SEL nextSelector;
-	if (iterator == NULL)
-		mlist = iterator = (void *) class_copyMethodList(class, (unsigned int *) &count);	// initialize
-	else if(count > 0)
-		// FIXME: shouldn't this be ((Method *) iterator)++
-		// but compiler rejects because it is not an lvalue
-		iterator++, count--;
-	else
+#if 1
+	NSLog(@"SenTestInvocationEnumerator nextObject: mlist=%p iterator=%p count=%lu", mlist, iterator, count);
+#endif
+	if(iterator == NULL)
+		mlist=iterator=(void *) class_copyMethodList(class, (unsigned int *) &count);	// initialize
+	if(count == 0)
 		{ // end of list
-		if(mlist)
-			free(mlist);
-		mlist=NULL;
-		return nil;
+			if(mlist)
+				free(mlist);
+			mlist=NULL;
+#if 1
+			NSLog(@"end of list");
+#endif
+			return nil;
 		}
 	nextSelector=method_getName(*(Method *)iterator);
+	// compiler rejects ((Method *) iterator)++ or +=1
+	iterator=((Method *) iterator)+1, count--;
+#if 1
+	NSLog(@"selector = %@", NSStringFromSelector(nextSelector));
+#endif
 	invocation=[NSInvocation invocationWithMethodSignature:[class instanceMethodSignatureForSelector:nextSelector]];
 	[invocation setSelector:nextSelector];
 	return invocation;
 #else
-    if (mlist == NULL) {
-        return nil;
-    }
-    else {
-        SEL nextSelector = mlist->method_list[count].method_name;
-        count--;
-        if (count == -1) {
-            [self goNextMethodList];
-        }
-        if (sel_isMapped(nextSelector)) {
-//			NSLog(@"SenTestInvocationEnumerator nextObject: create invocation for %@", NSStringFromSelector(nextSelector));
-            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[class instanceMethodSignatureForSelector:nextSelector]];
-            [invocation setSelector:nextSelector];
-            return invocation;
-        }
-        else {
-            return [self nextObject];
-        }
-    }
+	if (mlist == NULL) {
+		return nil;
+	}
+	else {
+		SEL nextSelector = mlist->method_list[count].method_name;
+		count--;
+		if (count == -1) {
+			[self goNextMethodList];
+		}
+		if (sel_isMapped(nextSelector)) {
+#if 0
+			NSLog(@"SenTestInvocationEnumerator nextObject: create invocation for %@", NSStringFromSelector(nextSelector));
+#endif
+			NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[class instanceMethodSignatureForSelector:nextSelector]];
+			[invocation setSelector:nextSelector];
+			return invocation;
+		}
+		else {
+			return [self nextObject];
+		}
+	}
 #endif
 }
 
 - (NSArray *) allObjects
 {
-    NSMutableArray *array = [NSMutableArray array];
-    id each;
+	NSMutableArray *array = [NSMutableArray array];
+	id each;
 
-    while ( (each = [self nextObject]) ) {
-        [array addObject:each];
-    }
-    return array;
+	while ( (each = [self nextObject]) ) {
+		[array addObject:each];
+	}
+	return array;
 }
 
 @end
