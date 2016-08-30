b89c682299ac080e842c124cec9d628d53722116
H. Nikolaus Schaller <hns@goldelico.com>
2016-08-30 18:16:35 +0200

    add NSBundleTest
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

---
+++ UnitTesting/Foundation/NSBundleTest.m
@@ -0,0 +1,64 @@
+//
+//  NSBundleTest
+//  UnitTests
+//
+//  Created by H. Nikolaus Schaller on 30.08.16.
+//  Copyright 2016 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface NSBundleTest : SenTestCase {
+	NSBundle *b;
+}
+
+@end
+
+@implementation NSBundleTest
+
+- (void) setUp
+{
+	b=[NSBundle bundleWithPath:@"/System/Library/Frameworks/AddressBook.framework"];
+	STAssertNotNil(b, nil);
+}
+
+- (void) tearDown
+{
+	[b release];
+}
+
+- (void) test01
+{
+	STAssertEquals([b bundlePath], @"/System/Library/Frameworks/AddressBook.framework");
+}
+
+- (void) test02
+{
+	// check executable path
+}
+
+// type
+// principalClass
+// infoDictionary
+// if a framework is becoming listed in [NSBundle frameworks]
+
+- (void) test03
+{
+}
+
+- (void) test04
+{
+}
+
+- (void) test05
+{
+	// check if methods from superclasses can be accessed
+	ABPerson *p=[[ABPerson alloc] init];
+	NSLog(@"superclass methods accessible: %d", [p respondsToSelector:@selector(forwardInvocation:)]);
+	NSLog(@"class: %@", [p class]);
+	NSLog(@"superclass: %@", [p superclass]);
+	[p release];
+}
+
+@end
