76a2d243eeb018b8b6d1ee65240856d6280c4b52
H. Nikolaus Schaller <hns@goldelico.com>
2016-10-24 15:36:17 +0200

    make Quartz.framework more similar to an Umbrella framework
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

---
+++ PDFKit/Quartz.m
@@ -0,0 +1,14 @@
+//
+//  Quartz.m
+//  PDFKit wrapper
+//
+//  Created by Dr. H. Nikolaus Schaller on Fri Dec 6 2005.
+//  Copyright (c) 2005 DSITRI. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+static void _Quartz_dummy(void)
+{
+	[PDFDocument class];	// reference PDFKit
+}
