caec5fd3fb559e904172304ba945902347c5a3d1
H. Nikolaus Schaller <hns@goldelico.com>
2017-05-07 10:35:51 +0200

    add XCTest.framework
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

---
+++ XCTest/XCTest.h
@@ -0,0 +1,13 @@
+//
+//  XCTest.h
+//
+//  Created by H. Nikolaus Schaller on 07.05.17.
+//  Copyright 2013 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+// This is a wrapper around SenTestingKit to provide the newer XCTest API
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#define XCTest SenTest
+#define XCTestCase SenTestCase
