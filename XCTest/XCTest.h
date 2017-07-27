61f8e31bd233b69237de8cbca62e8349cc31de89
H. Nikolaus Schaller <hns@goldelico.com>
2017-07-27 10:50:49 +0200

    XCTest: fix for macros
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- XCTest/XCTest.h
+++ XCTest/XCTest.h
@@ -4,7 +4,7 @@
 //  Created by H. Nikolaus Schaller on 07.05.17.
 //  Copyright 2013 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
 //
-// This is a wrapper around SenTestingKit to provide the newer XCTest API
+//  This is a wrapper around SenTestingKit to provide the newer XCTest API
 //
 
 #import <SenTestingKit/SenTestingKit.h>
@@ -12,11 +12,11 @@
 #define XCTest SenTest
 #define XCTestCase SenTestCase
 
-#define XCTAssertTrue(C)			STAssertTrue(C, nil)
-#define XCTAssertFalse(C)			STAssertFalse(C, nil)
-#define XCTAssertNotNil(O)			STAssertNotNil(O, nil)
-#define XCTAssertNil(O)				STAssertNil(O, nil)
-#define XCTAssertEqual(A, B)		STAssertEquals(A, B, nil)
-#define XCTAssertEqualObjects(A, B)	STAssertEqualObjects(A, B, nil)
-#define XCTAssertThrows(E)			STAssertThrows(E, nil)
-#define XCTAssertNoThrow(E)			STAssertNoThrow(E, nil)
+#define XCTAssertTrue(C, ...)			STAssertTrue(C, __VA_ARGS__)
+#define XCTAssertFalse(C, ...)			STAssertFalse(C, __VA_ARGS__)
+#define XCTAssertNotNil(O, ...)			STAssertNotNil(O, __VA_ARGS__)
+#define XCTAssertNil(O, ...)			STAssertNil(O, __VA_ARGS__)
+#define XCTAssertEqual(A, B, ...)		STAssertEquals(A, B, __VA_ARGS__)
+#define XCTAssertEqualObjects(A, B, ...)	STAssertEqualObjects(A, B, __VA_ARGS__)
+#define XCTAssertThrows(E, ...)			STAssertThrows(E, __VA_ARGS__)
+#define XCTAssertNoThrow(E, ...)		STAssertNoThrow(E, __VA_ARGS__)
