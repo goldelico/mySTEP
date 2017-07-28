3baa08a8af294c096302395b2ed61c175fad58dc
H. Nikolaus Schaller <hns@goldelico.com>
2017-07-28 14:59:22 +0200

    XCTest: make framework compile
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- XCTest/XCTest.h
+++ XCTest/XCTest.h
@@ -5,6 +5,7 @@
 //  Copyright 2013 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
 //
 //  This is a wrapper around SenTestingKit to provide the newer XCTest API
+//  The special gcc only syntax ##__VA_ARGS__ allows to omit the ... part
 //
 
 #import <SenTestingKit/SenTestingKit.h>
@@ -12,11 +13,15 @@
 #define XCTest SenTest
 #define XCTestCase SenTestCase
 
-#define XCTAssertTrue(C, ...)			STAssertTrue(C, __VA_ARGS__)
-#define XCTAssertFalse(C, ...)			STAssertFalse(C, __VA_ARGS__)
-#define XCTAssertNotNil(O, ...)			STAssertNotNil(O, __VA_ARGS__)
-#define XCTAssertNil(O, ...)			STAssertNil(O, __VA_ARGS__)
-#define XCTAssertEqual(A, B, ...)		STAssertEquals(A, B, __VA_ARGS__)
-#define XCTAssertEqualObjects(A, B, ...)	STAssertEqualObjects(A, B, __VA_ARGS__)
-#define XCTAssertThrows(E, ...)			STAssertThrows(E, __VA_ARGS__)
-#define XCTAssertNoThrow(E, ...)		STAssertNoThrow(E, __VA_ARGS__)
+#define XCTAssertTrue(C, ...)			STAssertTrue(C, ## __VA_ARGS__)
+#define XCTAssertFalse(C, ...)			STAssertFalse(C, ##__VA_ARGS__)
+#define XCTAssertNotNil(O, ...)			STAssertNotNil(O, ##__VA_ARGS__)
+#define XCTAssertNil(O, ...)			STAssertNil(O, ##__VA_ARGS__)
+#define XCTAssertEqual(A, B, ...)		STAssertEquals(A, B, ##__VA_ARGS__)
+#define XCTAssertEqualObjects(A, B, ...)	STAssertEqualObjects(A, B, ##__VA_ARGS__)
+#define XCTAssertThrows(E, ...)			STAssertThrows(E, ##__VA_ARGS__)
+#define XCTAssertNoThrow(E, ...)		STAssertNoThrow(E, ##__VA_ARGS__)
+#define XCTAssertThrowsSpecific(E, C, ...) \
+										STAssertThrowsSpecific(E, C, ##__VA_ARGS__)
+#define XCTAssertThrowsSpecificNamed(E, C, N, ...) \
+										STAssertThrowsSpecificNamed(E, C, N, ##__VA_ARGS__)
---
+++ XCTest/XCTest.m
@@ -0,0 +1,12 @@
+//
+//  XCTest.m
+//
+//  Created by H. Nikolaus Schaller on 28.07.17.
+//  Copyright 2013 Golden Delicious Computers GmbH&Co. KG. All rights reserved.
+//
+//  This is a wrapper around SenTestingKit to provide the newer XCTest API
+//
+
+#import <XCTest/XCTest.h>
+
+/* empty */
