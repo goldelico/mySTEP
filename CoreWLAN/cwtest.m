c6ba62e7ea8a5ce9a425ce5a50b8a6b616d5d1a3
H. Nikolaus Schaller <hns@goldelico.com>
2018-01-25 14:18:30 +0100

    CoreWLAN: cwtest: loop until networks have been successfully scanned
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- CoreWLAN/cwtest.m
+++ CoreWLAN/cwtest.m
@@ -14,25 +14,32 @@ int main(int argc, char *argv[])
 	NSAutoreleasePool *arp=[[NSAutoreleasePool alloc] init];
 	NSError *err;
 	CWInterface *cw=[CWInterface interface];
+	NSArray *nw;
+	NSEnumerator *e;
+	CWNetwork *network;
 	if(![cw setPower:YES error:&err])
 		{
 		NSLog(@"WLAN power on error: %@", err);
 		exit(1);
 		}
 	NSLog(@"cwtest: started - interface=%@", cw);
-	NSArray *nw=[cw scanForNetworksWithParameters:nil error:&err];
-	CWNetwork *network;
-	NSEnumerator *e=[nw objectEnumerator];
+	NSLog(@"cwtest: add unused port to keep runloop active!");
+	[[NSRunLoop mainRunLoop] addPort:[NSPort port] forMode:NSDefaultRunLoopMode];	// we must at least have one entry in the loop or -run will fail immediately
+	NSLog(@"cwtest: run loop");
+	while(YES)
+		{
+		nw=[cw scanForNetworksWithParameters:nil error:&err];
+		if(nw)
+			break;
+		[[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];	// run for another second
+		}
+	NSLog(@"cwtest: runloop did end with result!");
+	e=[nw objectEnumerator];
 	while((network=[e nextObject]))
 		{
 		NSString *str=[NSString stringWithFormat:@"%@: %ld dBm", [network ssid], (long)[network rssiValue]];
 		printf("%s\n", [str UTF8String]);
 		}
-	NSLog(@"cwtest: add unused port to keep runloop active!");
-	[[NSRunLoop mainRunLoop] addPort:[NSPort port] forMode:NSDefaultRunLoopMode];	// we must at least have one entry in the loop or -run will fail immediately
-	NSLog(@"cwtest: run loop");
-	[[NSRunLoop mainRunLoop] run];
-	NSLog(@"cwtest: runloop did end!");
 #if 0
 	// turn off only if it was already off
 	if(![cw setPower:NO error:&err])
