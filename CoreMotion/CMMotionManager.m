2ff514b86e12ac20916c3db1eb4361f27a84ccd0
H. Nikolaus Schaller <hns@goldelico.com>
2018-05-14 15:56:33 +0200

    CoreMotion: locate iio accelerometer and read scale and values
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- CoreMotion/CMMotionManager.m
+++ CoreMotion/CMMotionManager.m
@@ -55,16 +55,56 @@
 	static CMDeviceMotion *_deviceMotion;
 	if(!_deviceMotion)
 		_deviceMotion=[CMDeviceMotion new];
-	// if not yet located, locate iio devices
-	// read iio values
-	// store values in _deviceMotion object
-#if 1
+#if __linux__
+#if 0
 	NSLog(@"read iio accelerometers");
 #endif
-#define K 0.4
+	static NSString *accel;
+	static double scale;
+	if(!accel)
+		{
+		NSError *error;
+		NSString *dir=@"/sys/bus/iio/devices/";
+		NSEnumerator *e=[[[NSFileManager defaultManager] contentsOfDirectoryAtPath:dir error:&error] objectEnumerator];
+		NSString *device;
+		while((device=[e nextObject]))
+			{
+			device=[dir stringByAppendingPathComponent:device];
+#if 0
+			NSLog(@"try %@", device);
+#endif
+			id sc=[NSString stringWithContentsOfFile:[device stringByAppendingPathComponent:@"in_accel_scale"]];
+			if(sc)
+				{
+				scale=[sc doubleValue]/9.81;	// iio returns kg/s^2
+				accel=[device retain];
+				break;
+				}
+			}
+#if 0
+		NSLog(@"iio accel found: %@ scale=%g", accel, scale);
+#endif
+		}
+	if(accel)
+		{
+		_deviceMotion->_gravity.x=scale*[[NSString stringWithContentsOfFile:[accel stringByAppendingPathComponent:@"in_accel_x_raw"]] intValue];
+		_deviceMotion->_gravity.y=scale*[[NSString stringWithContentsOfFile:[accel stringByAppendingPathComponent:@"in_accel_y_raw"]] intValue];
+		_deviceMotion->_gravity.z=scale*[[NSString stringWithContentsOfFile:[accel stringByAppendingPathComponent:@"in_accel_z_raw"]] intValue];
+		// swap&scale depending on device model adjustments
+		// should read the Device database...
+		// GTA04A5:
+		_deviceMotion->_gravity.x=-_deviceMotion->_gravity.x;
+		}
+#else
+	// debugging
+#define K 0.6
 	_deviceMotion->_gravity.x=K*((float)rand()/((float)RAND_MAX)-0.5);
 	_deviceMotion->_gravity.y=K*((float)rand()/((float)RAND_MAX)-0.5);
 	_deviceMotion->_gravity.z=1;
+#endif
+#if 0
+	NSLog(@"accel x: %g y:%g z:%g", _deviceMotion->_gravity.x, _deviceMotion->_gravity.y, _deviceMotion->_gravity.z);
+#endif
 	return _deviceMotion;
 }
 
