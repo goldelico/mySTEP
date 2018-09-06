65bddf458417814582222b36ec43800a696bf555
H. Nikolaus Schaller <hns@goldelico.com>
2018-09-06 19:45:17 +0200

    AppKit: NSImage: add testing code that can draw a yellow frame around destRect; and other code that overrides all NIB loading
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- AppKit/Sources/NSImage.m
+++ AppKit/Sources/NSImage.m
@@ -35,6 +35,30 @@
 // Class variables 
 static NSMutableDictionary *__nameToImageDict = nil;
 
+#if 0	// debugging
+
+@implementation NSApplication (NSImage)
+
+- (void) finishLaunching
+{ // override NIB loading
+	NSImage *img=[NSImage imageNamed:@"NSToolbarShowFonts"];
+	NSWindow *w=[[NSWindow alloc] initWithContentRect:NSMakeRect(100, 100, 400, 200) styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
+	NSImageView *iv=[[NSImageView alloc] initWithFrame:NSMakeRect(50, 50, 100, 100)];
+#if 0	// currently, unflipped drawing works, flipped fails
+	[img setFlipped:YES];
+#endif
+	// set other image attributes
+	[iv setImage:img];
+	[iv setImageScaling:NSImageScaleNone];
+	// set other image view attributes, e.g.  resizing, frame, bounds, rotation
+	[w setContentView:iv];
+	[w makeKeyAndOrderFront:nil];
+}
+
+@end
+
+#endif
+
 @implementation NSImage
 
 + (void) initialize
@@ -510,6 +534,10 @@ static NSMutableDictionary *__nameToImageDict = nil;
 	if(NSIsEmptyRect(dest))
 		src.size=_size;	// use image size
 	[ctx saveGraphicsState];
+#if 0
+	[[NSColor yellowColor] set];
+	NSFrameRect(dest);
+#endif
 	co=[ctx compositingOperation];	// save
 	[ctx setCompositingOperation:op];
 	[ctx _setFraction:fraction];
