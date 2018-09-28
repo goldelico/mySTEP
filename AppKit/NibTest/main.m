5a782aa41dc5166febac2edb9c98e3aaeb56fe7b
H. Nikolaus Schaller <hns@goldelico.com>
2018-09-28 10:54:33 +0200

    AppKit: NibTest: improved NSImage drawing test code
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- AppKit/NibTest/main.m
+++ AppKit/NibTest/main.m
@@ -11,39 +11,59 @@ int main(int argc, const char *argv[])
 	return NSApplicationMain(argc, argv);
 }
 
-#if 0	// debugging
+#if 0	// for debugging of image drawing
 
 @implementation NSBundle (NSNibLoading)
 
 + (NSImage *) img
 {
+	NSString *path;
 #if 0
-	NSImage *img=[NSImage imageNamed:@"NSToolbarShowColors"];
+	path=@"/usr/local/QuantumSTEP/System/Sources/Frameworks/AppKit/Images/NSToolbarShowColors.icns";
 #else
-	NSImage *img=[NSImage imageNamed:@"NSToolbarShowFonts"];
+	path=@"/usr/local/QuantumSTEP/System/Sources/Frameworks/AppKit/Images/NSToolbarShowFonts.icns";
 #endif
-#if 0 // currently, flipped drawing is reversed
+	NSImage *img=[[[NSImage alloc] initWithContentsOfFile:path] autorelease];
+	if(!img)
+		{
+		NSLog(@"image not found!");
+		return nil;
+		}
+
+#if 0 // flipped drawing
 	[img setFlipped:YES];
 #endif
+
 #if 1	// test drawing/copying into other pixmap and not to screen
+	{
 	NSImage *copy=[[NSImage alloc] initWithSize:[img size]];
+	NSRect rect=(NSRect) { NSZeroPoint, [copy size] };
 	[copy lockFocus];
-#if 1
-	NSAffineTransform *atm=[NSAffineTransform transform];
-	[atm rotateByDegrees:-10.0];
-	[atm concat];	// apply rotation
+#if 1	// image rotation
+	{
+		NSAffineTransform *atm=[NSAffineTransform transform];
+		[atm rotateByDegrees:-15.0];
+		[atm concat];	// apply rotation before drawing
+	}
 #endif
-	[img drawInRect:NSZeroRect fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
+	[[NSColor redColor] set];
+	NSRectFill(rect);	// prefill background
+	[img drawInRect:rect fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
 	[copy unlockFocus];
-#if 0
-	NSImage *copy2=[[NSImage alloc] initWithSize:[copy size]];
+	img=[copy autorelease];
+	}
+#endif
+
+#if 0	// copy a second time
+	{
+	NSImage *copy2=[[NSImage alloc] initWithSize:[img size]];
 	[copy2 lockFocus];
-	[copy drawInRect:NSZeroRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+	[img drawAtPoint:NSZeroPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
 	[copy2 unlockFocus];
-	return copy2;
+	img=[copy2 autorelease];
+	}
 #endif
-	return copy;
-#endif
+
 	return img;
 }
 
@@ -51,15 +71,33 @@ int main(int argc, const char *argv[])
 
 + (BOOL) loadNibNamed:(NSString *) name owner:(id) owner
 {
-	NSWindow *w=[[NSWindow alloc] initWithContentRect:NSMakeRect(100, 100, 400, 200) styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
+	NSWindow *w=[[NSWindow alloc] initWithContentRect:NSMakeRect(100, 100, 250, 200) styleMask:(NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask) backing:NSBackingStoreBuffered defer:NO];
 	NSImageView *iv=[[NSImageView alloc] initWithFrame:NSMakeRect(50, 50, 100, 100)];
+	BOOL fl=[iv isFlipped];
+	NSLog(@"fl=%d", fl);
+
 	[w setTitle:@"NSImage Drawing Test"];
-	// set other image attributes
 	[iv setImage:[self img]];
 	[iv setImageScaling:NSImageScaleNone];
-	// set other image view attributes, e.g.  resizing, frame, bounds, rotation
+#if 1
+	[iv setImageScaling:NSImageScaleProportionallyUpOrDown];
+#endif
+#if 0
+	[iv setFrameRotation:10.0];
+#endif
+#if 0
+	[iv setBoundsRotation:10.0];
+	NSLog(@"%@", NSStringFromRect([iv frame]));
+#endif
+#if 1	// keep frame size
+	[[w contentView] addSubview:iv];
+#else	// adjust to window frame
 	[w setContentView:iv];
+#endif
+	NSLog(@"%@", NSStringFromRect([iv frame]));
+	[iv release];
 	[w makeKeyAndOrderFront:nil];
+	// [w autorelease];	// don't release or it will be closed...!
 	return YES;
 }
 
