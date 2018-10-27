83092224af05d22ca2f175f3d6a4550e0e67613f
H. Nikolaus Schaller <hns@goldelico.com>
2018-10-27 09:28:34 +0200

    IOBluetooth: make iobttest work on MacOS
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- IOBluetooth/iobttest.m
+++ IOBluetooth/iobttest.m
@@ -16,7 +16,7 @@
 					   aborted:(BOOL) aborted;
 {
 	NSLog(@"%@ error=%d aborted=%d", self, error, aborted);
-	NSLog(@"devices=%@", [sender foundDevices]);
+	//	NSLog(@"devices=%@", [sender foundDevices]);
 	_done=YES;
 }
 
@@ -25,7 +25,7 @@
 int main(int argc, char *argv[])
 {
 	NSAutoreleasePool *arp=[[NSAutoreleasePool alloc] init];
-	NSError *err;
+	//	NSError *err;
 #ifdef __mySTEP__
 	BOOL poweron=[IOBluetoothDeviceInquiry _bluetoothHardwareIsActive];
 	if(![IOBluetoothDeviceInquiry _activateBluetoothHardware:YES])
@@ -46,6 +46,10 @@ int main(int argc, char *argv[])
 		[[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];	// run for another second
 		}
 	NSLog(@"iobttest: runloop did end with result!");
+	NSEnumerator *e=[[inq foundDevices] objectEnumerator];
+	IOBluetoothDevice *dev;
+	while((dev=[e nextObject]))
+		printf("%s", [[dev description] UTF8String]);
 #ifdef __mySTEP__
 	if(![IOBluetoothDeviceInquiry _activateBluetoothHardware:poweron])
 		{ // turn off only if it was off before
