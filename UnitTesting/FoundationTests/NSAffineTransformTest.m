6027e2332b66a9ad8d6d68703f1652df312b9b58
H. Nikolaus Schaller <hns@goldelico.com>
2019-07-07 18:06:07 +0200

    UnitTesting: NSAffineTransform: make them pass again on Cocoa
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- UnitTesting/FoundationTests/NSAffineTransformTest.m
+++ UnitTesting/FoundationTests/NSAffineTransformTest.m
@@ -17,8 +17,8 @@
 
 // FIXME: apply withAccuracy to comparing NSPoints and NSSize!
 
-#define XCTAssertEqualPointsWithAccuracy(p1, p2, acc, msg) XCTAssertEqualWithAccuracy(p1.x, p2.x, acc, msg); XCTAssertEqualWithAccuracy(p1.y, p2.y, acc, msg);
-#define XCTAssertEqualSizesWithAccuracy(s1, s2, acc, msg) XCTAssertEqualWithAccuracy(s1.width, s2.width, acc, msg); XCTAssertEqualWithAccuracy(s1.height, s2.height, acc, msg);
+#define XCTAssertEqualPointsWithAccuracy(p1, p2, acc...) XCTAssertEqualWithAccuracy(p1.x, p2.x, acc); XCTAssertEqualWithAccuracy(p1.y, p2.y, acc);
+#define XCTAssertEqualSizesWithAccuracy(s1, s2, acc...) XCTAssertEqualWithAccuracy(s1.width, s2.width, acc); XCTAssertEqualWithAccuracy(s1.height, s2.height, acc);
 
 @implementation NSAffineTransformTest
 
@@ -50,12 +50,12 @@
 	NSAffineTransformStruct ts;
 	[t translateXBy:5.0 yBy:7.0];
 	ts=[t transformStruct];
-	XCTAssertEqual(ts.m11, (CGFloat) 1.0, @"");
-	XCTAssertEqual(ts.m12, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.m21, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.m22, (CGFloat) 1.0, @"");
-	XCTAssertEqual(ts.tX, (CGFloat) 5.0, @"");
-	XCTAssertEqual(ts.tY, (CGFloat) 7.0, @"");
+	XCTAssertEqual(ts.m11, (CGFloat) 1.0);
+	XCTAssertEqual(ts.m12, (CGFloat) 0.0);
+	XCTAssertEqual(ts.m21, (CGFloat) 0.0);
+	XCTAssertEqual(ts.m22, (CGFloat) 1.0);
+	XCTAssertEqual(ts.tX, (CGFloat) 5.0);
+	XCTAssertEqual(ts.tY, (CGFloat) 7.0);
 }
 
 - (void) test03
@@ -63,12 +63,12 @@
 	NSAffineTransformStruct ts;
 	[t rotateByDegrees:30.0];
 	ts=[t transformStruct];
-	XCTAssertEqualWithAccuracy(ts.m11, (CGFloat) 0.8660254, 2e-6, @"");
-	XCTAssertEqual(ts.m12, (CGFloat) 0.5, @"");
-	XCTAssertEqual(ts.m21, (CGFloat) -0.5, @"");
-	XCTAssertEqualWithAccuracy(ts.m22, (CGFloat) 0.8660254, 2e-6, @"");
-	XCTAssertEqual(ts.tX, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.tY, (CGFloat) 0.0, @"");
+	XCTAssertEqualWithAccuracy(ts.m11, (CGFloat) 0.8660254, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m12, (CGFloat) 0.5, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m21, (CGFloat) -0.5, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m22, (CGFloat) 0.8660254, 2e-6);
+	XCTAssertEqual(ts.tX, (CGFloat) 0.0);
+	XCTAssertEqual(ts.tY, (CGFloat) 0.0);
 }
 
 - (void) test04
@@ -77,12 +77,12 @@
 	[t translateXBy:5.0 yBy:7.0];
 	[t rotateByDegrees:30.0];
 	ts=[t transformStruct];
-	XCTAssertEqualWithAccuracy(ts.m11, (CGFloat) 0.8660254, 2e-6, @"");
-	XCTAssertEqual(ts.m12, (CGFloat) 0.5, @"");
-	XCTAssertEqual(ts.m21, (CGFloat) -0.5, @"");
-	XCTAssertEqualWithAccuracy(ts.m22, (CGFloat) 0.8660254, 2e-6, @"");
-	XCTAssertEqual(ts.tX, (CGFloat) 5.0, @"");
-	XCTAssertEqual(ts.tY, (CGFloat) 7.0, @"");
+	XCTAssertEqualWithAccuracy(ts.m11, (CGFloat) 0.8660254, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m12, (CGFloat) 0.5, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m21, (CGFloat) -0.5, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m22, (CGFloat) 0.8660254, 2e-6);
+	XCTAssertEqual(ts.tX, (CGFloat) 5.0);
+	XCTAssertEqual(ts.tY, (CGFloat) 7.0);
 }
 
 - (void) test05
@@ -91,12 +91,12 @@
 	[t rotateByDegrees:30.0];
 	[t translateXBy:5.0 yBy:7.0];
 	ts=[t transformStruct];
-	XCTAssertEqualWithAccuracy(ts.m11, (CGFloat) 0.8660254, 2e-6, @"");
-	XCTAssertEqual(ts.m12, (CGFloat) 0.5, @"");
-	XCTAssertEqual(ts.m21, (CGFloat) -0.5, @"");
-	XCTAssertEqualWithAccuracy(ts.m22, (CGFloat) 0.8660254, 2e-6, @"");
-	XCTAssertEqualWithAccuracy(ts.tX, (CGFloat) 0.830127, 2e-6, @"");
-	XCTAssertEqualWithAccuracy(ts.tY, (CGFloat) 8.562178, 2e-6, @"");
+	XCTAssertEqualWithAccuracy(ts.m11, (CGFloat) 0.8660254, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m12, (CGFloat) 0.5, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m21, (CGFloat) -0.5, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.m22, (CGFloat) 0.8660254, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.tX, (CGFloat) 0.830127, 2e-6);
+	XCTAssertEqualWithAccuracy(ts.tY, (CGFloat) 8.562178, 2e-6);
 }
 
 - (void) test10
@@ -105,7 +105,7 @@
 	[t rotateByDegrees:30.0];
 	[t translateXBy:5.0 yBy:7.0];
 	pt=[t transformPoint:NSMakePoint(10.0, 15.0)];
-	XCTAssertEqualPointsWithAccuracy(pt, NSMakePoint(1.99038, 26.552559), 2e-6, @"");
+	XCTAssertEqualPointsWithAccuracy(pt, NSMakePoint(1.99038, 26.552559), 2e-6);
 }
 
 - (void) test11
@@ -114,7 +114,7 @@
 	[t rotateByDegrees:30.0];
 	[t translateXBy:5.0 yBy:7.0];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(4.820507, 31.650635), 2e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(4.820507, 31.650635), 2e-6);
 }
 
 - (void) test12
@@ -123,7 +123,7 @@
 	[t rotateByDegrees:30.0+180.0];
 	[t translateXBy:5.0 yBy:7.0];
 	pt=[t transformPoint:NSMakePoint(10.0, 15.0)];
-	XCTAssertEqualPointsWithAccuracy(pt, NSMakePoint(-1.99038, -26.552559), 2e-6, @"");
+	XCTAssertEqualPointsWithAccuracy(pt, NSMakePoint(-1.99038, -26.552559), 2e-6);
 }
 
 - (void) test13
@@ -132,7 +132,7 @@
 	[t rotateByDegrees:30.0+180];
 	[t translateXBy:5.0 yBy:7.0];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820508, -31.650635), 2e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820508, -31.650635), 2e-6);
 }
 
 - (void) test20
@@ -140,7 +140,7 @@
 	NSSize sz;
 	[t scaleXBy:2.0 yBy:-3.0];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(40.0, -75.0), 2e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(40.0, -75.0), 2e-6);
 }
 
 - (void) test30
@@ -152,7 +152,7 @@
 	[t appendTransform:t2];
 	[t translateXBy:5.0 yBy:7.0];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6);
 }
 
 - (void) test31
@@ -164,7 +164,7 @@
 	[t translateXBy:5.0 yBy:7.0];
 	[t appendTransform:t2];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6);
 }
 
 - (void) test32
@@ -176,7 +176,7 @@
 	[t prependTransform:t2];
 	[t translateXBy:5.0 yBy:7.0];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6);
 }
 
 - (void) test33
@@ -188,7 +188,7 @@
 	[t translateXBy:5.0 yBy:7.0];
 	[t prependTransform:t2];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6);
 }
 
 - (void) test34
@@ -200,7 +200,7 @@
 	[t rotateByDegrees:180.0];
 	[t prependTransform:t2];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6);
 }
 
 - (void) test35
@@ -212,7 +212,7 @@
 	[t rotateByDegrees:180.0];
 	[t appendTransform:t2];
 	sz=[t transformSize:NSMakeSize(20.0, 25.0)];
-	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6, @"");
+	XCTAssertEqualSizesWithAccuracy(sz, NSMakeSize(-4.820505, -31.650635), 4e-6);
 }
 
 - (void) test40
@@ -222,7 +222,7 @@
 	[t translateXBy:5.0 yBy:7.0];
 	[t invert];
 	pt=[t transformPoint:NSMakePoint(1.99038, 26.5526)];
-	XCTAssertEqualPointsWithAccuracy(pt, NSMakePoint(10.000021, 15.000036), 2e-6, @"");
+	XCTAssertEqualPointsWithAccuracy(pt, NSMakePoint(10.000021, 15.000036), 2e-6);
 }
 
 - (void) test41
@@ -230,13 +230,13 @@
 	NSAffineTransformStruct ts;
 	[t scaleBy:0.0];
 	ts=[t transformStruct];
-	XCTAssertEqual(ts.m11, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.m12, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.m21, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.m22, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.tX, (CGFloat) 0.0, @"");
-	XCTAssertEqual(ts.tY, (CGFloat) 0.0, @"");
-	XCTAssertThrowsSpecific([t invert], NSException, @"");
+	XCTAssertEqual(ts.m11, (CGFloat) 0.0);
+	XCTAssertEqual(ts.m12, (CGFloat) 0.0);
+	XCTAssertEqual(ts.m21, (CGFloat) 0.0);
+	XCTAssertEqual(ts.m22, (CGFloat) 0.0);
+	XCTAssertEqual(ts.tX, (CGFloat) 0.0);
+	XCTAssertEqual(ts.tY, (CGFloat) 0.0);
+	XCTAssertThrowsSpecific([t invert], NSException);
 }
 
 @end
