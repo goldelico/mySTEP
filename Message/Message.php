d6d32a80a45fbe91e86c48fdeda8f35317694ea5
H. Nikolaus Schaller <hns@goldelico.com>
2020-12-07 14:55:34 +0100

    Message.php: simple solution for sending messages with MIME attachments
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- Message/Message.php
+++ Message/Message.php
@@ -63,8 +63,21 @@ class NSMailDelivery extends NSObject
 	}
 
 // to send to multiple recipients, provide $headers['To'] as an array
-// to send MIME with attachments, use an attributed string
+// to send MIME with attachments, use an attributed string as $body
+// or an array as $body
+/*
+	example for sending mail with attachment
+	$headers=array('Subject' => "Here is your PDF attached", 'To' => "somebody@yourdomain.com");
+	$msg[]="Here is some PDF:";
+	$msg[]=(new PDFDocument())->initWithURL("https://server/file.pdf");
+	$msg[]="Hope your find it good.";
+	NSMailDelivery::deliverMessageHeadersFormatProtocol($msg, $headers, NSMailDelivery::NSMIMEMailFormat);
+*/
 
+	/*
+	 * $body must be a string for NSASCIIMailFormat
+	 * $body must be an array for NSMIMEMailFormat
+	 */
 
 	public static function deliverMessageHeadersFormatProtocol(/*NSAttributedString*/$body, array $headers, $format, $protocol=null)
 	{
@@ -84,7 +97,7 @@ class NSMailDelivery extends NSObject
 			// should check if To, Bcc, CC, Reply-To: are isValid
 			$hdrs.="$key: $value\r\n";	// convert
 			}
-		if($format == self::NSASCIIMailFormat)
+		if(!is_array($body) && $format == self::NSASCIIMailFormat)
 			// FIXME: convert attributed string to ASCII
 /* FIXME:
  * convert utf8 to 7-bit ascii
@@ -95,44 +108,53 @@ class NSMailDelivery extends NSObject
 			$msg=$body;
 		else
 			{
-
-/*****
-			// if there are attachments:
-
-			// specify MIME version 1.0
-			$hdrs .= "Mime-Version: 1.0\n";
-
-			// unique boundary
-			$boundary = uniqid("QuantumSTEP");
-
-			// tell e-mail client this e-mail contains alternate versions
-			$hdrs .= "Content-Type: multipart/mixed" . "; boundary=$boundary\n\n";
-
-			$msg="";
-
-			// loop over strings and attachments in NSAttributedString $body
+			// or check for NSAttributedString
+			if(count($body) > 1)
 				{
-				$string=...;
-				$infos[]=...;
+				// specify MIME version 1.0
+				$hdrs .= "Mime-Version: 1.0\n";
 
-				$msg .= "--$boundary\n";
-				// get from attachment infos
-				$msg .= "Content-Transfer-Encoding: quoted-printable\n";
-				$msg .= "Content-Type: text/plain; charset=ISO-8859-1\n";
-				// if we attach a PDF or something for download
-				$msg .= "Content-Disposition: attachment; something.pdf\n";
-				$body .= "\n";
+				// unique boundary
+				$boundary = uniqid("QuantumSTEP");
 
-				if(string)
-					$msg .= $string;
-				else
-					$msg .= chunk_split(base64_encode($attachment), 76, "\n");
+				// tell e-mail client this e-mail contains alternate versions
+				$hdrs .= "Content-Type: multipart/mixed" . "; boundary=$boundary\n\n";
 
-				$msg .= "\n\n";
+				$msg="";
+
+				foreach($body as $part)
+					{
+					$msg .= "--$boundary\n";
+
+					if(is_object($part))
+						{
+						// must implement dataRepresentation() - like PDFDocument does
+						// and potentially some other methods to control the content type
+						// may also take NSData arguments...
+						$msg .= "Content-Transfer-Encoding: base64\n";
+						// FIXME: how do we known pdf? - at the moment we simply assume
+						$msg .= "Content-Type: application/pdf; charset=UTF-8\n";
+						$msg .= "Content-Disposition: attachment; somefile.pdf\n";
+						$msg .= "\n";
+						$msg .= chunk_split(base64_encode($part->dataRepresentation()), 76, "\n");
+						$msg .= "\n";
+						}
+					else
+						{ // string
+						$msg .= "Content-Transfer-Encoding: quoted-printable\n";
+						$msg .= "Content-Type: text/plain; charset=ISO-8859-1\n";
+						$msg .= "\n";
+						$msg .= $part;
+						$msg .= "\n";
+						}
+						// get from attachment infos and type from $body
+
+					$msg .= "\n";
+					}
+				$msg .= "\n--$boundary--";	// final boundary
 				}
-			$msg .= "\n--$boundary--";	// final boundary
-
-*****/
+			else
+				$msg=$body[0];
 			}
 // get error: error_get_last()['message']
 		return mail($headers['To'], $headers['Subject'], $msg, $hdrs);
