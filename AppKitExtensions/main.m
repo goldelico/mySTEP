bbf9290bd56d65c1761ce687eab024b6b8602d18
H. Nikolaus Schaller <hns@goldelico.com>
2020-12-13 17:39:27 +0100

    AppKitExtensions: add NSScaleRotateFlipViewDemo
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

---
+++ AppKitExtensions/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ElectroniCAD
+//
+//  Created by H. Nikolaus Schaller on 20.11.09.
+//  Copyright Golden Delicious Computers GmbH&Co. KG 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	/* install catch-all exception handler like described here: https://www.cocoawithlove.com/2010/05/handling-unhandled-exceptions-and.html */
+	return NSApplicationMain(argc, (const char **) argv);
+}
