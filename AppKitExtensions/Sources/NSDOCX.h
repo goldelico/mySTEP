bd486653f0823d0d784d42651455685bc79d9c00
H. Nikolaus Schaller <hns@goldelico.com>
2021-06-01 07:47:54 +0200

    AppKitExtensions: experimental start for NSDoc and NSXIBLoading
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

---
+++ AppKitExtensions/Sources/NSDOCX.h
@@ -0,0 +1,19 @@
+//
+//  NSDOCX.h
+//  AppKitExtensions
+//
+//  Created by H. Nikolaus Schaller on 09.03.21.
+//
+//
+
+#import <AppKit/AppKit.h>
+
+NSString *NSDocumentTypeDocumentAttribute=@"DocumentType";
+NSString *NSOfficeOpenXMLTextDocumentType;	// ECMA Office Open XML text document format.
+
+@interface NSAttributedString (DOCX)
+
+- (id) initWithDocFormat:(NSData *) data documentAttributes:(NSDictionary **) dict;
+- (NSData *) dataFromRange:(NSRange) range documentAttributes:(NSDictionary *dict) error:**error;
+
+@end
---
+++ AppKitExtensions/Sources/NSDOCX.m
@@ -0,0 +1,27 @@
+//
+//  NSXIBLoading.m
+//  AppKitExtensions
+//
+//  Created by H. Nikolaus Schaller on 09.03.21.
+//
+//
+
+#import "NSXIBLoading.h"
+
+NSString *NSDocumentTypeDocumentAttribute=@"DocumentType";
+NSString *NSOfficeOpenXMLTextDocumentType@".docx";	// ECMA Office Open XML text document format
+													// denkbar wäre auch .odt (Apple TextEdit kann alle diese Formate im Save... Dialog)
+
+@implementation NSAttributedString (DOCX)
+
+- (id) initWithDocFormat:(NSData *) data documentAttributes:(NSDictionary **) dict;
+{
+	// for a quick description (not the standard) see: https://www.toptal.com/xml/an-informal-introduction-to-docx
+}
+
+- (NSData *) dataFromRange:(NSRange) range documentAttributes:(NSDictionary *dict) error:**error;
+{
+
+}
+
+@end
