8a85b1333120f2f5a5dce57387f148674d1ef9c2
H. Nikolaus Schaller <hns@goldelico.com>
2023-01-18 21:59:26 +0100

    IOBluetooth: include CoreFoundation for OSStatus and others
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- IOBluetooth/BluetoothAssignedNumbers.h
+++ IOBluetooth/BluetoothAssignedNumbers.h
@@ -7,36 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
-// FIXME: not sure if this all should really be declared here or somewhere else
-
-#ifdef __mySTEP__
-
-// declares types from libkern/IOTypes.h
-
-typedef unsigned int UInt;
-typedef signed int SInt;
-typedef unsigned char UInt8;
-typedef unsigned short UInt16;
-typedef unsigned long UInt32;
-typedef unsigned long long UInt64;
-typedef signed char SInt8;
-typedef signed short SInt16;
-typedef signed long SInt32;
-typedef signed long long SInt64;
-typedef SInt32 OSStatus;
-typedef UInt32 OptionBits;
-typedef unsigned char Boolean;
-
-// declares from IOKit/IOReturn.h
-
-typedef int IOReturn;
-typedef UInt32 IOItemCount;
-
-#define kIOReturnSuccess 0
-#define kIOReturnError 1
-
-#endif
+#import <CoreFoundation/CoreFoundation.h>
 
 #ifndef NIMP
 #define NIMP ([NSException raise:@"Not implemented" format:@"method %@ not implemented in %@", NSStringFromSelector(_cmd), NSStringFromClass([self class])], (void *) 0)
