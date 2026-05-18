e40b0439f3b93891338d17db889f03b743301b91
H. Nikolaus Schaller <hns@goldelico.com>
2026-05-18 17:21:46 +0200

    CoreDataBase.php: fix fgetcsv/fputcsv parameters; remove OLD code
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- CoreDataBase/CoreDataBase.php
+++ CoreDataBase/CoreDataBase.php
@@ -11,153 +11,13 @@ require_once "$ROOT/System/Library/Frameworks/Foundation.framework/Versions/Curr
 
 if($GLOBALS['debug']) echo "<h1>CoreDataBase.framework</h1>";
 
-/* OLD
-
-class ManagedObjectContext extends NSObject
-{ // represents a data storage
-}
-
-class ManagedObjectEntity extends NSObject
-
-// Eigentlich EntityDescription?
-
-{
-	protected $context;	// database connection
-	protected $table;	// the table we represent
-// Keys und Abfragen sollten durch das Datenmodell beschrieben werden!
-	protected $primaryKey;	// name of primary key column
-
-	protected $foreignEntity;	// to build a chain/network (?) of entities
-	
-	public function get($key, $fetch=false)
-		{ // fetch a single value for the given key
-echo "get ".htmlentities($key)."<p>";
-		$mo=new ManagedObject($this, $key);
-		if($fetch)
-			$mo->fetch();
-		if(!exists)
-			$mo=$this->newObject();		// create new entry
-		return $mo;
-		}
-	
-	public function newObject()
-		{ // create new empty entry with uuid key
-echo "new<p>";
-		// make uuid or unique index
-			$key=uuid();
-			$mo=new ManagedObject($this, $key);
-			return $mo;
-		}
-
-	// Zugriff als NSTableDataSource erlauben?
-
-// das sollte ŸberflŸssig sein!
-// genaugenommen ist das ein (beschreibbarer) SQL-View was wir hier versuchen nachzubilden
-
-	public function getBySQL($where=NULL, $order=NULL, $group=NULL, $limit=0)
-		{
-		$query="select * from `$this->_table`";
-		return $mo;
-		}
-
-	public function getByColumn($colname, $key, $limit=0)
-		{
-		return $this->getBySQL("`$colname` = '$key'", NULL, NULL, $limit);
-		}
-
-/*
-	public function fields()
-		{
-		for($i=0; $i<mysql_num_fields($result); $i=$i+1)
-			{
-			$f=mysql_field_name($result, $i);
-			}
-		}
-* /
-
-	public function constructor($context, $table, $primarykey)
-		{
-		$this->context=$context;
-		$this->table=$table;
-		$this->primarykey=$primarykey;
-		}
-
-}
-
-class ManagedObject extends NSObject
-{ // represents a single row of a database
-	protected $entity;
-	protected $key;
-	protected $timestamp;
-	protected $row;
-	protected $fetched;
-	protected $dirty;
-
-	public function fetch()
-		{
-		echo "query ".htmlentities($fetch)."<p>";
-		if($this->_fetched)
-			return;
-		$this->row=$this->entity->getByColumn($this->entity->primarykey, $this->key, 1);
-		}
-
-	public function flush()
-		{
-		if($this->dirty)
-		// use $this->entity->quote("");
-			$this->entity->query("update `".$this->entity->table."` set `$prop` = '$value'... where `$_row`='$key'");
-		$this->dirty=false;
-		}
-
-	public function key()
-		{
-		return $this->_key;
-		}
-
-	public function setKey($key)
-		{
-		$this->flush();
-		$this->key=$key;
-		$this->fetched=false;
-		}
-
-	public function __set($prop, $value)
-		{
-		echo "__set ".htmlentities($prop)." = ".htmlentities($value)."<p>";
-		$this->fetch();
-		$this->row[$prop] = $value;
-		$this->dirty=true;	// we could collect columns that have been changed
-		// we can also notify the context that this entity has been updated
-		}
-
-	public function __get($prop)
-		{
-		echo "__get ".htmlentities($prop)."<p>";
-		$this->fetch();
-		return (isset($this->row[$prop])) ? $this->row[$prop] : null;
-		}
-
-	public function __construct($entity, $key)
-		{
-      		parent::__construct();
-		$this->entity=$entity;
-		$this->key=$key;
-		}
-
-	public function __destruct()
-		{
-		$this->flush();
-		}
-
-}
-
-OLD */
-
 class _CSV
 {
 	protected $file;
 	protected $separator;
 	protected $enclosure;
+	protected $escape;
+	protected $eol;
 	protected $columns;
 	protected $rows;
 	static $cache;
@@ -205,6 +65,7 @@ class _CSV
 		$this->file=$directory."/".$file;
 		$this->separator=",";
 		$this->enclosure="\"";
+		$this->escape="\\";
 		if(substr($file, -4) == ".tsv")
 			{
 			$this->separator="\t";
@@ -261,12 +122,12 @@ class _CSV
 			NSLog("could not read $this->file");
 			return false;
 			}
-		if(($this->columns = fgetcsv($handle, 0, $this->separator, $this->enclosure)) === false)
+		if(($this->columns = fgetcsv($handle, 0, $this->separator, $this->enclosure, $this->escape)) === false)
 			return false;
 // _NSLog($this->separator);
 // _NSLog($this->columns);
 		$this->rows[]=array();
-		while(($values = fgetcsv($handle, 0, $this->separator, $this->enclosure)) !== false)
+		while(($values = fgetcsv($handle, 0, $this->separator, $this->enclosure, $this->escape)) !== false)
 			{
 // print_r($values);
 			$row=array();
@@ -322,13 +183,13 @@ class _CSV
 			fclose($handle);
 			return true;	// OK
 			}
-		if(!fputcsv($handle, $this->columns, $this->separator, $this->enclosure))
+		if(!fputcsv($handle, $this->columns, $this->separator, $this->enclosure, $this->escape, $this->eol))
 			{
 			fclose($handle);
 			return false;
 			}
 		foreach($this->rows as $record)
-			if(!fputcsv($handle, $record, $this->separator, $this->enclosure))
+			if(!fputcsv($handle, $record, $this->separator, $this->enclosure, $this->escape, $this->eol))
 				{
 				fclose($handle);
 				return false;
@@ -375,6 +236,8 @@ class _CSV
 		$this->file=$file;
 		$this->separator=",";
 		$this->enclosure="\"";
+		$this->escape="\\";
+		$this->eol="\n";
 		$this->columns=$columns;
 		$this->rows=array();	// empty
 	}
